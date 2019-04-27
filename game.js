3047171bc94d51d74dfe91444e13d1f1a41d6ec0
Kevin Zheng <coffeevector@gmail.com>
2019-04-26 22:37:29 -0700

    Moved game code out of the html
    The svgs are going to be hard coded into the html, and that's not going
    to be pretty to work witt h

--- game.js
+++ game.js
@@ -1,10 +1,13 @@
 var elem = document.getElementById('draw-shapes');
-var params = { width: 285, height: 200 };
+var params = { width: 285, height: 200, fullscreen:true };
 var two = new Two(params).appendTo(elem);
+
+var line = new Two.Line(0, 0, 20, 25);
 
 // two has convenience methods to create shapes.
 var circle = two.makeCircle(72, 100, 50);
 var rect = two.makeRectangle(213, 100, 100, 100);
+
 
 // The object returned has many stylable properties:
 circle.fill = '#FF8000';
@@ -18,3 +21,26 @@ rect.noStroke();
 // Don't forget to tell two to render everything
 // to the screen
 two.update();
+
+var rails = [new Two.Vector(3, 4), new Two.Vector(5, -3)]
+var speed = 1;
+var spawn = new Two.Vector(150, 150);
+var death = function(e) {
+
+}
+var enemy = new Enemy(speed, spawn, rails, death, rect);
+
+two.bind('update', function(frameCount) {
+	if(keys["Control"]) {
+		panSpeed.addSelf(mouseDelta.clone().multiplyScalar(-.5));
+	}
+	panSpeed.divideScalar(1.1);
+	if(panSpeed.isZero()) {
+		panSpeed.clear();
+	}
+
+	two.scene.translation.addSelf(panSpeed);
+	enemy.update(two.timeDelta);
+
+}).play();  // Finally, start the animation loop
+
