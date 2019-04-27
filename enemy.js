8e9e369b428b0f2e23724e8bf163b5d7f36dad2c
kevinPal <kevinpalani@gmail.com>
2019-04-27 00:53:46 -0500

    non acc rails

--- enemy.js
+++ enemy.js
@@ -2,11 +2,11 @@ class Enemy {
 
     update(deltaTime) {
         this.d += this.speed * (deltaTime / 1000);
-        console.log(this);
         var currentRail = this.rails[this.currentRail]
         var currentRailLen = currentRail.length();
         if(this.d > currentRailLen) {
             console.log("Swapping rails");
+            this.vecRailPos.add(this.vecRailPos, currentRail);
             this.d -= currentRailLen;
             this.currentRail += 1;
             currentRail = this.rails[this.currentRail]
@@ -16,8 +16,8 @@ class Enemy {
         }
 
         var angle = Math.atan2(currentRail.y, currentRail.x);
-        this.position.add(this.vecRailPos, new Two.Vector(this.d * Math.cos(angle), this.d * Math.sin(angle)));
-
+        
+        this.position = new Two.Vector(this.vecRailPos.x + this.d * Math.cos(angle), this.vecRailPos.y + this.d * Math.sin(angle));
 
         this.twoObject.translation.copy(this.position);
     }
--- game.js
+++ game.js
@@ -4,17 +4,19 @@ var two = new Two(params).appendTo(elem);
 
 var line = new Two.Line(0, 0, 20, 25);
 
-var pig = two.interpret(document.getElementById('pig'))
-
+//var pig = two.interpret(document.getElementById('pig'))
+var rect = two.makeRectangle(213, 100, 100, 100);
 two.update();
 
-var rails = [new Two.Vector(3, 4), new Two.Vector(5, -3)]
-var speed = 1;
+var rails = [new Two.Vector(300, 0), new Two.Vector(0, 100),
+new Two.Vector(200, 0), new Two.Vector(0, 500),
+new Two.Vector(-300, 0), new Two.Vector(0, 100)]
+var speed = 100;
 var spawn = new Two.Vector(150, 150);
 var death = function(e) {
 
 }
-var enemy = new Enemy(speed, spawn, rails, death, pig);
+var enemy = new Enemy(speed, spawn, rails, death, rect);
 
 two.bind('update', function(frameCount) {
 	if(keys["Control"]) {
